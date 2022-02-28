c1a10740aa5f607e5375c6f4dcac87cb6271e05e
SHiN201007 <shin1007m@gmail.com>
2022-02-28 13:48:49 +0900

    gitignore 作成

--- Pods/Target Support Files/Pods-SampleCompositionalLayout-SampleCompositionalLayoutUITests/Pods-SampleCompositionalLayout-SampleCompositionalLayoutUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SampleCompositionalLayout_SampleCompositionalLayoutUITests : NSObject
-@end
-@implementation PodsDummy_Pods_SampleCompositionalLayout_SampleCompositionalLayoutUITests
-@end
--- Pods/Target Support Files/Pods-SampleCompositionalLayoutTests/Pods-SampleCompositionalLayoutTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SampleCompositionalLayoutTests : NSObject
-@end
-@implementation PodsDummy_Pods_SampleCompositionalLayoutTests
-@end
